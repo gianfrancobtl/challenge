55dc50e7799592427702133cf39f84f4c8a9c74d
Gian <gianfrancobtl@gmail.com>
2022-02-03 00:19:06 -0300

    Testing percentages and withdrawals

--- ETHPool/contracts/EthereumPool.sol
+++ ETHPool/contracts/EthereumPool.sol
@@ -2,20 +2,91 @@
 
 pragma solidity ^0.6.6;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EthereumPool is Ownable {
+contract EthereumPool {
+    event EthDeposited(address _from, uint256 _amount);
+
+    address public owner;
+
     uint256 totalDeposit;
+    address[] public depositors;
 
     mapping(address => uint256) addressToDeposits;
+    mapping(address => uint256) addressToPoolParticipation;
 
-    function deposit_rewards() public onlyOwner {}
-
-    function deposit_ETH() public {}
-
-    function take_out_deposits() public {
-        // require() que los fondos sean retirados por la address correcta.
+    constructor() public {
+        owner = msg.sender;
     }
 
-    function give_out_rewards() private {}
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function deposit_rewards() public payable onlyOwner {
+        distribute_rewards(msg.value);
+    }
+
+    function deposit_ETH() public payable {
+        if (!isDepositor(msg.sender)) {
+            depositors.push(msg.sender);
+        }
+        totalDeposit += msg.value;
+        addressToDeposits[msg.sender] += msg.value;
+        emit EthDeposited(msg.sender, msg.value);
+        updateDataBase();
+    }
+
+    function withdraw_deposits() public payable {
+        require(
+            addressToDeposits[msg.sender] >= msg.value,
+            "You must deposit some Ether in order to withdraw."
+        );
+        msg.sender.transfer(
+            (addressToPoolParticipation[msg.sender] * address(this).balance) /
+                10000
+        );
+        addressToDeposits[msg.sender] -= msg.value;
+        totalDeposit -= msg.value;
+        updateDataBase();
+    }
+
+    function updateDataBase() private {
+        for (uint256 i = 0; i < depositors.length; i++) {
+            addressToPoolParticipation[depositors[i]] =
+                (addressToDeposits[depositors[i]] * 10000) /
+                totalDeposit;
+        }
+    }
+
+    function distribute_rewards(uint256 _amount) private {
+        for (uint256 i = 0; i < depositors.length; i++) {
+            addressToDeposits[depositors[i]] +=
+                (_amount * addressToPoolParticipation[depositors[i]]) /
+                10000;
+        }
+    }
+
+    function getDeposits(address _depositor) public view returns (uint256) {
+        return addressToDeposits[_depositor];
+    }
+
+    function getPoolParticipation(address _depositor)
+        public
+        view
+        returns (uint256)
+    {
+        return addressToPoolParticipation[_depositor];
+    }
+
+    function isDepositor(address _depositor) private view returns (bool) {
+        bool res = false;
+        for (uint256 i = 0; i < depositors.length; i++) {
+            if (depositors[i] == _depositor) {
+                res = true;
+            }
+        }
+        return res;
+    }
 }
--- ETHPool/scripts/helpful_script.py
+++ ETHPool/scripts/helpful_script.py
@@ -1,13 +1,19 @@
-from brownie import network, accounts, config
+""" 
 
+get_account():
+1) Si la cuenta que realiza el deploy esta en los ambientes locales,
+retornar la primera cuenta del vector de cuentas.
+2) Caso contrario (testnet o mainnet) importar la cuenta desde config mediante PRIVATE_KEY. 
+ 
+"""
+
+from brownie import network, accounts, config
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = [
     "development",
 ]
 
-# 1) Si la cuenta que realiza el deploy esta en los ambientes locales,
-# retornar la primera cuenta del vector de cuentas.
-# 2) Caso contrario (testnet o mainnet) importar la cuenta desde config mediante PRIVATE_KEY.
+
 def get_account():
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
